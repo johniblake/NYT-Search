21d57aa60b98cd39b80a7b1dba05b618998d6f63
johniblake <johniblake93@gmail.com>
2019-05-03 13:53:47 -0500

    hooked up search button to listener

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,36 +3,63 @@ const API_KEY = "Aw6KSGUh9yHFlHc450TWoJT8TyQDZEpu";
 let filter = {
   numRecords: 10,
   startYear: "2012",
-  endYear: "2019",
+  endYear: "",
   term: "election"
 };
-let filterString = "";
 
-let queryURL =
-  "https://api.nytimes.com/svc/search/v2/articlesearch.json?" +
-  filterString +
-  "&api-key=" +
-  API_KEY;
+function displayResults(response) {
+  let resultsContainer = $("<div>");
+  resultsContainer.attr("id", "results");
+  let results = response.response.docs;
 
-function getFilterString() {
-  if (filter.startYear !== "") {
-    filterString += "&begin_date=" + filter.startYear + "0101";
-  }
-  if (filter.endYear !== "") {
-    filterString += "&end_date=" + filter.startYear + "0101";
-  }
-  if (filter.term !== "") {
-    filterString += "&fq=" + filter.term;
+  for (let i = 0; i < numRecords; i++) {
+    let article = results[i];
+    let title = article.headline.main;
+    let byline = article.byline.original;
+    let pubDate = article.pub_date;
+    let url = article.web_url;
+    let artivleDiv = $("<div>");
   }
 }
 
-getFilterString();
+function getFilterString() {
+  let filterString = "";
+  if (filter.startYear !== "") {
+    filterString = filterString + "&begin_date=" + filter.startYear + "0101";
+  }
+  if (filter.endYear !== "") {
+    filterString = filterString + "&end_date=" + filter.endYear + "0101";
+  }
+  if (filter.term !== "") {
+    filterString = filterString + "&q=" + filter.term;
+  }
+  return filterString;
+}
+$(document).ready(function() {
+  $("#search").on("click", query);
+});
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-})
-  // execute this function after observable returned
-  .then(function(response) {
-    console.log(response);
-  });
+function query() {
+  filter.term = $("#term").val();
+  filter.startYear = $("#start-year").val();
+  filter.endYear = $("#end-year").val();
+
+  let filterString = getFilterString();
+  console.log("QUERY STRING: " + filterString);
+
+  let queryURL =
+    "https://api.nytimes.com/svc/search/v2/articlesearch.json?" +
+    filterString +
+    "&sort=relevance&api-key=" +
+    API_KEY;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // execute this function after observable returned
+    .then(function(response) {
+      console.log(response);
+      displayResults(response);
+    });
+}
